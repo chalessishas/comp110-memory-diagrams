a766175d3199e78da214086f564607ddb1012098
邵强 <shaoq@shaoqiangdeMacBook-Air.local>
2026-02-18 20:39:04 -0500

    use PORT env variable

--- app.py
+++ app.py
@@ -28,4 +28,5 @@ def translate():
     return jsonify({"result": response.content[0].text})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host='0.0.0.0', port=port)
